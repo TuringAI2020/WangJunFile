7a8108674d358389a979c693fe1035d4a59bb3af
俊 汪 <turingai@outlook.com>
2019-08-15 07:18:17 +0800

    添加文件遍历器

---
+++ WangJun.File/FILE.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WangJun.Yun
+{
+    public class FILE
+    {
+        protected Stack<string> folderStack = new Stack<string>();
+        public int StackDeep {
+            get {
+                return this.folderStack.Count;
+            }
+        }
+        public void AddFolder(string folderPath)
+        {
+            this.folderStack.Push(folderPath);
+        }
+        public RES Traverse(string rootPath, EventHandler<CallbackEventAgrs> fileCallback, EventHandler<CallbackEventAgrs> foldlerCallback)
+        {
+            if (!string.IsNullOrWhiteSpace(rootPath))
+            {
+                if (Directory.Exists(rootPath))
+                {
+
+                    this.AddFolder(rootPath);
+                    while (0 < this.folderStack.Count)
+                    {
+                        try
+                        {
+                            var cur = this.folderStack.Pop();
+                            var files = Directory.EnumerateFiles(cur);
+
+
+                            foreach (var file in files)
+                            {
+                                if (null != fileCallback)
+                                {
+                                    fileCallback(this, new CallbackEventAgrs { DATA = file });
+                                }
+                            }
+
+                            var subFolders = Directory.EnumerateDirectories(cur);
+
+                            foreach (var folder in subFolders)
+                            {
+                                if (null != foldlerCallback)
+                                {
+                                    foldlerCallback(this, new CallbackEventAgrs { DATA = folder });
+                                }
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine($"{ex.Message}");
+                        }
+
+                    }
+
+                }
+            }
+            return RES.OK();
+        }
+    }
+}
---
+++ WangJun.FileAPP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WangJun.Yun;
+
+namespace WangJun.FileAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            new FILE().Traverse(@"D:\", (sender, e) =>
+            {
+                var THIS = sender as FILE;
+                Console.WriteLine($"{THIS.StackDeep}\t{++count}\t{e.DATA}");
+            }, (sender, e) =>
+            {
+                var THIS = sender as FILE;
+                THIS.AddFolder(e.DATA as string);
+
+            });
+
+            Console.ReadKey();
+
+        }
+    }
+}
